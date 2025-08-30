6554ea837bb9f72ad567746280deee2132e39bb1
Blaine Curren <blaine.curren@gmail.com>
2025-08-30 11:12:31 -0500

    added frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 const port = 3000;
+
+app.use(express.json());
+
+app.post('/alert', (req, res) => {
+ const {crypto, current_vol, avg_vol, threshold} = req.body;
+  io.emit('trade_update', {crypto, current_vol, avg_vol, threshold});  
+  res.sendStatus(200);
+});
 
 app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Backend listening at http://localhost:${port}`);
 });
